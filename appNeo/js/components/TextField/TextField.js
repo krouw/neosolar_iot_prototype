8fcb498e2f844e1f4d70c7f0f9ba5cfed3cf06d0
krouw <felipe.andrez@live.cl>
2017-04-15 20:11:57 -0300

    Agregando animaciones TextField

--- appNeo/js/components/TextField/TextField.js
+++ appNeo/js/components/TextField/TextField.js
@@ -1,26 +1,107 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-import TextInput from 'react-native-md-textinput'
+import React, { Component, PropTypes } from 'react';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
+import Underline from './Underline';
+import FloatingLabel from './FloatingLabel';
 
-import { styles } from './styles'
+class TextField extends Component {
 
-const TextField = ({
-  input: { onChange , ...restInput },
-  meta: { asyncValidating, touched, error, invalid },
-  isPassword,
-  label }) => {
-    console.log(invalid);
-  return (
-    <View style={styles.container}>
-      <TextInput
-          label={label}
-          style={styles.text}
-          onChangeText={onChange}
-          highlightColor={touched && error ? 'red' : 'green' }
-          secureTextEntry={isPassword ? isPassword : false}
-          {...restInput} />
-      {touched && (error && <Text style={styles.errInput}>{error}</Text>)}
-    </View>);
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    let {
+      input: { value, onChange, onFocus, onBlur, ...restInput },
+      meta: { asyncValidating, touched, error, invalid, active },
+      isPassword,
+      label,
+      duration,
+      borderColor,
+      ...props,
+    } = this.props;
+    return (
+      <View style={[styles.container]}>
+        <View style={[styles.wrapperInput]}>
+          <TextInput
+              style={[styles.textInput]}
+              onChangeText={onChange}
+              onFocus={(event) => {
+                this.refs.label.floatLabel();
+                this.refs.underline.expandLine();
+                onFocus(event);
+              }}
+              onBlur={(event) => {
+                !value.length ? this.refs.label.sinkLabel() : null;
+                error ? this.refs.underline.expandLine() : this.refs.underline.shrinkLine() ;
+                onBlur(event);
+              }}
+              secureTextEntry={isPassword ? isPassword : false}
+              {...restInput}
+              {...props} />
+          <FloatingLabel
+            ref='label'
+            label={'prueba'}
+            labelColor={'gray'}
+            highlightColor={'blue'}
+            isFocused={active} />
+          <Underline
+            ref="underline"
+            duration={duration}
+            borderColor={'gray'}
+            highlightColor={'blue'} />
+        </View>
+        <View style={styles.wrapperErr}>
+          {touched && (error && <Text style={styles.textErr}>{error}</Text>)}
+        </View>
+      </View>);
+  }
 }
+
+const styles = StyleSheet.create({
+    container: {
+      marginBottom: 16,
+    },
+    wrapperInput:{
+      height: 72,
+      paddingTop: 38,
+      paddingBottom: 7,
+      position: 'relative',
+    },
+    textInput:{
+      width: 300,
+      height: 34,
+      fontSize: 16,
+      lineHeight: 34,
+    },
+    wrapperErr:{
+      height: 16,
+    },
+    textErr:{
+      paddingRight: 16,
+      color: 'red',
+    },
+    test :{
+     borderStyle: 'solid',
+     borderColor: 'red',
+     borderWidth: 1,
+    },
+});
+
+TextField.propTypes = {
+  input: PropTypes.object,
+  meta: PropTypes.object,
+  isPassword: PropTypes.bool,
+  label: PropTypes.string,
+};
+
+TextField.defaultProps = {
+  duration: 200,
+  labelColor: '#9E9E9E',
+  borderColor: 'red',
+  textColor: '#000',
+  dense: false,
+  underlineColorAndroid: 'rgba(0,0,0,0)',
+};
+
 
 export default TextField
---
+++ appNeo/js/components/TextInput/TextInput.js
@@ -0,0 +1,158 @@
+'use strict';
+import React, {Component, PropTypes} from "react";
+import {View, TextInput, StyleSheet} from "react-native";
+
+import Underline from './Underline';
+import FloatingLabel from './FloatingLabel';
+
+export default class TextField extends Component {
+  constructor(props: Object, context: Object) {
+    super(props, context);
+    this.state = {
+      isFocused: false,
+      text: props.value
+    };
+  }
+  focus() {
+    this.refs.input.focus();
+  }
+  blur() {
+    this.refs.input.blur();
+  }
+  isFocused() {
+    return this.state.isFocused;
+  }
+
+  render() {
+    let {
+      label,
+      highlightColor,
+      duration,
+      labelColor,
+      borderColor,
+      textColor,
+      textFocusColor,
+      textBlurColor,
+      onFocus,
+      onBlur,
+      onChangeText,
+      value,
+      dense,
+      inputStyle,
+      wrapperStyle,
+      labelStyle,
+      error,
+      ...props
+    } = this.props;
+    return (
+      <View style={[dense ? styles.denseWrapper : styles.wrapper, wrapperStyle]} ref="wrapper">
+        <TextInput
+          style={[dense ? styles.denseTextInput : styles.textInput, {
+            color: textColor
+          }, (this.state.isFocused && textFocusColor) ? {
+            color: textFocusColor
+          } : {}, (!this.state.isFocused && textBlurColor) ? {
+            color: textBlurColor
+          } : {}, inputStyle]}
+          onFocus={() => {
+            this.setState({isFocused: true});
+            this.refs.floatingLabel.floatLabel();
+            this.refs.underline.expandLine();
+            onFocus && onFocus();
+          }}
+          onBlur={() => {
+            this.setState({isFocused: false});
+            !this.state.text.length && this.refs.floatingLabel.sinkLabel();
+            console.log(error);
+            if(!error){
+              this.refs.underline.shrinkLine();
+            }
+            onBlur && onBlur();
+          }}
+          onChangeText={(text) => {
+            this.setState({text});
+            onChangeText && onChangeText(text);
+          }}
+          ref="input"
+          value={this.state.text}
+          {...props}
+        />
+        <Underline
+          ref="underline"
+          highlightColor={highlightColor}
+          duration={duration}
+          borderColor={borderColor}
+        />
+        <FloatingLabel
+          isFocused={this.state.isFocused}
+          ref="floatingLabel"
+          focusHandler={this.focus.bind(this)}
+          label={label}
+          labelColor={labelColor}
+          highlightColor={highlightColor}
+          duration={duration}
+          dense={dense}
+          hasValue={(this.state.text.length) ? true : false}
+          style={labelStyle}
+        />
+      </View>
+    );
+  }
+}
+
+TextField.propTypes = {
+  duration: PropTypes.number,
+  label: PropTypes.string.isRequired,
+  highlightColor: PropTypes.string,
+  labelColor: PropTypes.string,
+  borderColor: PropTypes.string,
+  textColor: PropTypes.string,
+  textFocusColor: PropTypes.string,
+  textBlurColor: PropTypes.string,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func,
+  onChangeText: PropTypes.func,
+  value: PropTypes.string,
+  dense: PropTypes.bool,
+  inputStyle: PropTypes.object,
+  wrapperStyle: PropTypes.object,
+  labelStyle: PropTypes.object,
+  error: PropTypes.bool,
+};
+
+TextField.defaultProps = {
+  duration: 200,
+  labelColor: '#9E9E9E',
+  borderColor: '#E0E0E0',
+  textColor: '#000',
+  value: '',
+  dense: false,
+  underlineColorAndroid: 'rgba(0,0,0,0)',
+  error: false,
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    height: 72,
+    paddingTop: 30,
+    paddingBottom: 7,
+    position: 'relative'
+  },
+  denseWrapper: {
+    height: 60,
+    paddingTop: 28,
+    paddingBottom: 4,
+    position: 'relative'
+  },
+  textInput: {
+    fontSize: 16,
+    height: 34,
+    lineHeight: 34
+  },
+  denseTextInput: {
+    fontSize: 13,
+    height: 27,
+    lineHeight: 24,
+    paddingBottom: 3
+  }
+});
