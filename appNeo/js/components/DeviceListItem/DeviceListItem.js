9f96eb776011e8051fd4fe8bbe7ebf6249934c67
krouw <felipe.andrez@live.cl>
2017-04-30 06:31:23 -0300

    APP | add DeviceList Component y Estilos. add DeviceListItem component y estilos

---
+++ appNeo/js/components/DeviceListItem/DeviceListItem.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { View,
+         Text,
+         StyleSheet,
+         TouchableOpacity } from 'react-native'
+import IconMd from 'react-native-vector-icons/MaterialIcons';
+import IconIon from 'react-native-vector-icons/Ionicons';
+
+const DeviceListItem = ({data}) => {
+  return (
+    <TouchableOpacity
+      onPress={() => console.log('dakjd')}>
+      <View style={[styles.DeviceWrapper]}>
+        <IconMd
+          style={styles.storage}
+          name="storage"
+          color="gray"
+          size={40} />
+        <View style={[styles.device]}>
+          <Text style={styles.name}>
+            {data.name}
+          </Text>
+          <Text style={styles.status}>
+            {data.status}
+          </Text>
+        </View>
+        <View style={[styles.arrow]}>
+          <IconIon
+            name="ios-arrow-dropright"
+            color="gainsboro"
+            size={30} />
+        </View>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  DeviceWrapper: {
+    height: 80,
+    paddingLeft: 16,
+    paddingRight: 16,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  storage:{
+    flex: 1
+  },
+  device:{
+    flex: 3,
+  },
+  name:{
+    fontSize: 16,
+    lineHeight: 19,
+    fontWeight: '400',
+    color: '#424242'
+  },
+  status: {
+    fontSize: 14,
+    lineHeight: 16,
+  },
+  arrow: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  test :{
+   borderStyle: 'solid',
+   borderColor: 'red',
+   borderWidth: 1,
+  },
+});
+
+
+export default DeviceListItem
--- appNeo/js/containers/DeviceList/DeviceList.js
+++ appNeo/js/containers/DeviceList/DeviceList.js
@@ -1,37 +1,150 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { View, Text } from 'react-native'
+import { View,
+         Text,
+         StyleSheet,
+         TextInput,
+         ListView } from 'react-native'
 import { MKButton } from 'react-native-material-kit'
 import { Logout } from '../../actions/auth'
 import { Actions, ActionConst } from 'react-native-router-flux'
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import DeviceListItem from '../../components/DeviceListItem/DeviceListItem'
 
 const DeviceList = ({ Logout }) => {
-  const ColoredRaisedButton = MKButton.coloredButton()
-    .withStyle({width:260,height: 50,alignItems:'center', justifyContent:'center',})
+  const AccentIconButton = MKButton.accentColoredFlatButton()
     .build();
 
+  const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+  const data = {
+    dataSource: ds.cloneWithRows([
+      {name: 'Centro de Investigación 1',
+       status: 'active'},
+      {name: 'Utem ingenería 2',
+       status: 'active'},
+      {name: 'Casa 3',
+       status: 'active'},
+      {name: 'Un nombre bastante largo4',
+      status: 'es solo un estado'},
+      {name: 'Un nombre bastante largo5',
+      status: 'es solo un estado'},
+      {name: 'Un nombre bastante largo6',
+      status: 'es solo un estado'},
+      {name: 'Un nombre bastante largo7',
+      status: 'es solo un estado'},
+      {name: 'Un nombre bastante largo8',
+      status: 'es solo un estado'},
+      {name: 'Un nombre bastante largo9',
+      status: 'es solo un estado'}]),
+  };
+
   return(
-    <View>
-      <Text>DeviceList</Text>
-      <View>
-        <ColoredRaisedButton
-          onPress={(e) => {
-            Actions.signin({type: ActionConst.RESET})
-            Logout()
-          }}>
-            <Text
-              style={{color: 'white', fontWeight: 'normal', fontSize: 24,}}>
-              Salir
-            </Text>
-        </ColoredRaisedButton>
+    <View style={styles.container}>
+      <View style={[styles.header]}>
+        <Text style={styles.title}>Spots Fotovoltaicos</Text>
+        <AccentIconButton>
+          <Icon name="account-circle"
+            color="gray"
+            size={24} />
+        </AccentIconButton>
+      </View>
+      <View style={[styles.searchBar]}>
+        <View style={styles.searchBg}></View>
+        <View style={[styles.searchInputWrapper]}>
+          <View style={styles.searchInputContent}>
+            <Icon
+              name="search"
+              color="gray"
+              size={24} />
+            <TextInput
+              underlineColorAndroid={'rgba(0,0,0,0)'}
+              placeholder={'Buscar Spot Fotovoltaico'}
+              style={[styles.searchInput]} />
+          </View>
+        </View>
+      </View>
+      <View style={[styles.content]}>
+        <ListView
+          style={styles.deviceList}
+          dataSource={data.dataSource}
+          renderRow={(rowData) => <DeviceListItem data={rowData} />}
+          renderSeparator={(sectionId, rowId) => <View key={rowId} style={styles.separator}></View> }
+        />
       </View>
     </View>
   );
 }
 
-DeviceList.propTypes = {
-
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    height: 56,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 16,
+    paddingRight: 8,
+    backgroundColor: 'gainsboro',
+  },
+  title:{
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  searchBar: {
+    height: 50,
+    overflow: 'visible',
+    marginBottom: 4,
+  },
+  searchBg:{
+    backgroundColor: 'gray',
+    height: 50,
+    zIndex: 1,
+  },
+  searchInputWrapper: {
+    marginTop: 16,
+    width: '100%',
+    marginRight: 16,
+    position: 'absolute',
+    paddingRight: 16,
+    paddingLeft: 16,
+  },
+  searchInputContent:{
+    zIndex: 2,
+    height: 40,
+    elevation: 3,
+    paddingRight: 16,
+    paddingLeft: 16,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  searchInput: {
+    width: '85%',
+    marginLeft: 24,
+  },
+  content:{
+    flex: 1,
+    zIndex: 1,
+  },
+  deviceList: {
+    flex: 1,
+  },
+  separator: {
+   flex: 1,
+   height: StyleSheet.hairlineWidth,
+   backgroundColor: '#757272',
+   marginLeft: 16,
+   marginRight: 16,
+  },
+  test :{
+   borderStyle: 'solid',
+   borderColor: 'red',
+   borderWidth: 1,
+  },
+});
 
 function mapDispatchToProps(dispatch){
   return {
