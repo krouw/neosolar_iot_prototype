e20768df74e6226233bacc62f164e0937eb139b1
roinflames <rod.reyes.s@gmail.com>
2017-05-03 17:47:00 -0300

    ACCUWEATHER | Correccion sintaxis

--- apiREST/controllers/accu.js
+++ apiREST/controllers/accu.js
@@ -1,22 +1,17 @@
 import express from 'express';
-import validator from 'validator';
-import isEmpty from 'lodash/isEmpty';
-import axios from 'axios'
-import cheerio from 'cheerio'
 var accuweather = require('node-accuweather')()('HeVASZ1RAZuAji6Loywzn6RzjdpRis5W');
 
 class AccuController {
 
   getInfo(req, res){
-
     accuweather.getCurrentConditions("Santiago", {unit: "Celsius"})
-      .then(function(result) {
+      .then( result => {
+        console.log(result);
         return res.status(200).json(result)
+      })
       .catch( err => {
         return res.status(500).json({ message: 'Lo sentimos, Hubo un problema en responder tu solicitud.' });
       })
-    });
-  }
-
+    }
 }
 export default AccuController
