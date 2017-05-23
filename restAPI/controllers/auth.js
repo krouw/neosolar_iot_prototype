11ce041c10c8b172a0d1246797e709573cdd0c9f
krouw <felipe.andrez@live.cl>
2017-05-23 13:08:41 -0400

    REST | Fix AuthController validateUser

--- restAPI/controllers/auth.js
+++ restAPI/controllers/auth.js
@@ -9,54 +9,7 @@ import User from '../models/user';
 import Device from '../models/device';
 import { socialAuth, validateByGoole } from '../config/socialAuth'
 import { MONGO } from '../config/config';
-
-function validateUser(data, db){
-    let errors = {};
-
-    if(isEmpty(data.email)){
-      errors.email = 'Campo Requerido'
-    }
-    else{
-      if(!validator.isEmail(data.email)){
-        errors.email = 'Email inválido'
-      }
-    }
-
-    if (isEmpty(data.password)) {
-      errors.password = 'Campo Requerido'
-    }
-    else if (data.password.length<6 || data.password.length>20 ) {
-      errors.password = 'Contraseña de 6 a 20 caracteres'
-    }
-
-    if(db){
-      //Retorno una funcion (Promise) y recibo resultado
-      return User.findOne({ 'email' : data.email })
-        .then( user => {
-          if(user){
-            errors.email = 'Este email está siendo utilizado por un usuario'
-          }
-          return {
-            errors,
-            isValid: isEmpty(errors)
-          }
-        })
-        .catch((err) => {
-          errrors.server = 'Problemas con el servidor'
-          return {
-            errors,
-            isValid: isEmpty(errors)
-          }
-        })
-    }
-    else{
-      //Retorno un objeto
-      return {
-        errors,
-        isValid: isEmpty(errors)
-      }
-    }
-}
+import { validateUser } from '../libs/validate'
 
 function validateGoogle(data){
   let errors = {};
